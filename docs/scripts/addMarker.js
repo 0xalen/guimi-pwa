9bebb28be666d12fabc82cd74a4cd47adbbc9e89
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-11-13 10:02:16 -0300

    dynamically add marker

--- docs/scripts/addMarker.js
+++ docs/scripts/addMarker.js
@@ -1,28 +1,57 @@
-function addMarker() {
-    document.getElementById("debugP").innerHTML= "Entered function";                /* DEBUG*/
-    var sceneEl = document.querySelector('a-scene');
-    document.getElementById("debugP").innerHTML= "Scene Selected";              /* DEBUG*/
+/*		var m = document.querySelector("a-marker");
+        m.addEventListener("markerFound", function(){
+            var markerId = marker.id;
+            console.log('markerFound', markerId);
+            // Marker found
+            document.getElementById("debugP").innerHTML= "Marker Found!";
+            alert("Marker found: " + markerId);
+        });
+        m.addEventListener("markerLost", function{
+            var markerId = marker.id;
+            console.log('markerLost', markerId);
+            // Marker lost
+            document.getElementById("debugP").innerHTML= "Lost";
+            alert("Marker found: " + markerId);
+        });
+*/
+function addMarker(markerID, pattURL) {
+    var sceneEl = document.querySelector("a-scene");
     
     var markerEl = document.createElement('a-marker');
-    markerEl.setAttribute("id", "amarkerID");
+    markerEl.setAttribute("id", markerID);
     markerEl.setAttribute("preset", "pattern");
     markerEl.setAttribute("type", "pattern");
-    markerEl.setAttribute("url", "https://raw.githubusercontent.com/0xalen/guimi/master/visitante/test/pattern/PA0001.patt");
+    markerEl.setAttribute("url", pattURL);
     markerEl.setAttribute("registerevents", "");
-    document.getElementById("debugP").innerHTML= "Marker created + attributes setted";              /* DEBUG*/
-    
-    var imageEl = document.createElement('a-image'); 
+
+    //sceneEl.appendChild(markerEl);
+    sceneEl.insertAdjacentElement('beforeend', markerEl);
+    document.getElementById("debugP").innerHTML = markerEl.id;              /* DEBUG*/
+
+
+	addImage(markerID);
+}
+
+function addImage(markerID) {
+	var markerEl = document.getElementById(markerID);
+	document.getElementById("debugP").innerHTML = markerEl.id;              /* DEBUG*/
+	    
+	var imageEl = document.createElement('a-image'); 
     imageEl.setAttribute("position","0 1 0"); 
     imageEl.setAttribute("rotation", "-90 0 0");
     imageEl.setAttribute("width", "1");
     imageEl.setAttribute("height", "1");
     imageEl.setAttribute("src", "#transpImage");
-    document.getElementById("debugP").innerHTML= "Image created + attributes setted";               /* DEBUG*/
     
-    sceneEl.appendChild(markerEl);
-    document.getElementById("debugP").innerHTML= "Marker appended to scene";                /* DEBUG*/
-            
-    imageEl.appendChild(markerEl);
-    document.getElementById("debugP").innerHTML= "Image appended to Marker";                /* DEBUG*/
-    
+    markerEl.insertAdjacentElement('beforeend', imageEl);
+}
+
+/* document.getElementById("debugP").innerHTML= "Image appended to Marker";                /* DEBUG*/  
+
+function debugMarker() {
+	var div = document.getElementById("divDebug");
+	var para = document.createElement("p");
+	para.setAttribute("id", "testP");
+	para.setAttribute("registerevents", "");
+	div.appendChild(para);
 }
