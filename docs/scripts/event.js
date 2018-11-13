0287750d33a9c4dcf4501cff34c32b0e0d90efb1
Alejandro <24446995+0xalen@users.noreply.github.com>
2018-11-13 18:20:38 -0300

    DEBUG Lines for type and value of markerId

--- docs/scripts/event.js
+++ docs/scripts/event.js
@@ -1,18 +1,22 @@
 function findEvents() {
     var m = document.querySelector('a-marker');
-    console.log("m type: " + m.tagName);
+    console.log("m type: " + typeof m.tagName);        //DEBUG
     m.addEventListener("markerFound", function(){
         var markerId = marker.id;
+        console.log("id type: " +  typeof markerId);        //DEBUG
+        console.log("markerId: " + markerId);        //DEBUG
         console.log('markerFound', markerId);
         // Marker found
         document.getElementById("debugP").innerHTML = "MARKER FOUND!";
         alert("Marker found: " + markerId);
     });
     m.addEventListener("markerLost", function() {
-		var markerId = marker.id;
-		console.log('markerLost', markerId);
+        var markerId = marker.id;
+        console.log("id type: " +  typeof markerId);        //DEBUG
+        console.log("markerId: " + markerId);        //DEBUG
+        console.log('markerLost', markerId);
         // Marker lost
-		document.getElementById("debugP").innerHTML = "MARKER LOST!";
-		alert("Marker found: " + markerId);
-	});
+        document.getElementById("debugP").innerHTML = "MARKER LOST!";
+        alert("Marker found: " + markerId);
+    });
 }
